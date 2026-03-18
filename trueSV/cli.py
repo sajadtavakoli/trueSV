3bfeaeb69d00b909cedfd218ff3b91bf4529d1ee
Sajad Tavakoli <41271921+sajadtavakoli@users.noreply.github.com>
2026-03-18 15:56:26 +0100

    cli.py created

---
+++ trueSV/cli.py
@@ -0,0 +1,68 @@
+import argparse
+
+from .core.pipeline import run_trueSV
+from .version import __version__
+import torch.multiprocessing as mp
+import pysam
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="trueSV",
+        description="trueSV: trustworthy deep learning-based structural variant calling",
+    )
+    parser.add_argument('-a', '--aln_path', type=str, required=True , help='path of the alignment file (*.bam, *.cram)')
+    parser.add_argument('-r', '--ref_path', type=str, required=True, help='path of the reference genome (*.fa, *.fasta)')
+    parser.add_argument('-o', '--out_path', type=str, required=True, help='output path (*.vcf)')
+    parser.add_argument('-c', '--coverage', type=int, required=True, help='coverage of the alignment file')
+    parser.add_argument('-t', '--threads', type=int, default=4, help='number of threads')
+    parser.add_argument('-s', '--sample', type=str, default='SAMPLE', help='sample name')
+    parser.add_argument('--contigs', type=str, default='all', help='contigs-default-> all contigs (for example: chr1,chr2)')
+    parser.add_argument('--fast', type=int, default=1, help='fast mode is faster, but likely a bit drop in accuracy (default: 1, 1->fast, 0->slow)')
+    
+    parser.add_argument("--version", action="version", version=f"%(prog)s {__version__}")
+    args = parser.parse_args()
+
+    aln_path=args.aln_path
+    ref_path=args.ref_path
+    out_path=args.out_path
+    cov=args.coverage
+    threads=args.threads
+    sample=args.sample
+    contigs=args.contigs
+    fast=args.fast
+
+    aln = pysam.AlignmentFile(aln_path)
+    all_contigs = aln.references
+    all_lengths = aln.lengths
+
+    if contigs=='all':
+        contigs = all_contigs
+        lengths = all_lengths
+    else: 
+        contigs = contigs.split(',')
+        lengths = []
+        for idx, contig in enumerate(all_contigs):
+            if contig in contigs:
+                lengths.append(all_lengths[idx])
+    aln.close()
+
+
+
+    run_trueSV(
+        aln_path=aln_path,
+        ref_path=ref_path,
+        out_path=out_path,
+        cov=cov,
+        threads=threads,
+        sample=sample,
+        contigs=contigs,
+        lengths=lengths,
+        fast=fast,
+    )
+
+
+
+if __name__ == "__main__":
+    mp.set_start_method("spawn", force=True)
+    main()
